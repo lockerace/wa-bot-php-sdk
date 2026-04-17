ae7981390d154f22b8e4919e3b2a92091fd30041
Ace Locker <lockerace@gmail.com>
2026-04-17 08:21:20 +0800

    added: phone status & verbosity enum

---
+++ src/Models/WaBotPhoneStatus.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Lockerace\WaBot\Models;
+
+class WaBotPhoneStatus
+{
+    const ONLINE = 1;
+    const OFFLINE = 2;
+}
---
+++ src/Models/WaBotPhoneVerbosity.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Lockerace\WaBot\Models;
+
+class WaBotPhoneVerbosity
+{
+    const SILENT = 0;
+    const NORMAL = 1;
+}
